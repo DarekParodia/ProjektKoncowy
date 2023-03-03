a6f5a7f5319afb291943aef1d115e9d3722a49b1
DarekParodia <darekparodia@gmail.com>
2023-03-03 10:53:38 +0100

    adadaereadthtykyu

---
+++ js/default.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+    var mobileButt = document.getElementById("mobileButt");
+    var mobileButtKlikniety = false;
+    mobileButt.addEventListener("click", (e) => {
+        let cntr = 0;
+        for (const child of mobileButt.children) {
+            cntr++;
+            if (mobileButtKlikniety) child.setAttribute("class", `menu-div-pojda`);
+            else child.setAttribute("class", `menu-div-pojda-klikniete` + cntr);
+        }
+        mobileButtKlikniety = !mobileButtKlikniety;
+    });
+    console.log("loaded");
+});
