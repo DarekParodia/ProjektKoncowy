f997936e7fa6cb6510f40c5806821b607d1ecb99
DarekParodia <darekparodia@gmail.com>
2023-04-24 16:42:10 +0200

    basic server-client

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require("express");
-const gameHost = require("./js/host.js");
 const app = express();
 const port = 80;
 const gamePort = 8080;
-
-const server = new gameHost.host(gamePort);
+class host {
+    constructor(port, name, maxPlayers) {
+        this.app = express();
+        this.port = port;
+        this.name = name;
+        this.maxPlayers = maxPlayers;
+        this.players = [];
+        this.app.get("*", (req, res) => {
+            res.redirect("http://" + req.headers.host.split(":")[0] + req.url);
+        });
+        this.app.post("/ping", (req, res) => {
+            let data = req.body;
+            res.send("pong");
+        });
+        this.app.post("/gamepocket", (req, res) => {
+            let data = req.body;
+            res.send("gamepocket");
+        });
+        this.app.listen(this.port, () => {
+            console.log(`Game host running at: ${this.port}`);
+        });
+    }
+}
+var serverList = [];
 
 app.use("/js", express.static(__dirname + "/js"));
 app.use("/css", express.static(__dirname + "/css"));
@@ -17,7 +38,22 @@ app.get("/", (req, res) => {
 app.get("/main.html", (req, res) => {
     res.sendFile(__dirname + "/main.html");
 });
+app.get("/joingame", (req, res) => {
+    res.send(req.query);
+});
+app.post("/getserverlist", (req, res) => {
+    let data = req.body;
+    console.log("ServerList request reveived");
+    res.send(serverList);
+});
 
 app.listen(port, () => {
     console.log(`Strona aktywna pod portem: ${port}`);
+    createLobby("Main Server", 100);
+    for (let i = 0; i < 100; i++) createLobby("Test Server" + i, 0);
 });
+function createLobby(name, maxPlayers = 10) {
+    let server = new host(gamePort + serverList.length, name, maxPlayers);
+    serverList.push(server);
+    return server;
+}
